82a6a021fe150634335bb0dd403f2d9476b034c2
myt003 <155334207+myt003@users.noreply.github.com>
2025-05-05 10:08:06 +0100

    added admin seeder

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,49 @@
 
 namespace App\DataFixtures;
 
+use AllowDynamicProperties;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppFixtures extends Fixture
+#[AllowDynamicProperties] class AppFixtures extends Fixture
 {
+    public function __construct(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager)
+    {
+        $this->passwordHasher = $passwordHasher;
+        $this->entityManager = $entityManager;
+
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
-        $manager->flush();
+        //$manager->flush();
+
+
+        $existingAdmin = $this->entityManager->getRepository(User::class)->findOneBy(['roles' => ['ROLE_ADMIN']]);
+
+        if (!$existingAdmin) {
+
+            $admin = new User();
+            $admin->setEmail('admin@example.com');
+            $admin->setRoles(['ROLE_ADMIN']);
+            $admin->setFirstName('Admin');
+            $admin->setLastName('User');
+            $admin->setIsVerified(true);
+
+            // Hash password before saving
+            $hashedPassword = $this->passwordHasher->hashPassword($admin, 'admin003');
+            $admin->setPassword($hashedPassword);
+
+            $manager->persist($admin);
+            $manager->flush();
+        }
     }
+
+
 }
